700cf7a8e7d116a36c5ee54f4c93b1d1fd3e91d6
Prithvi Pal Singh <Prithvi.Coer@outlook.com>
2018-04-16 12:23:48 +0800

    Add project files.

---
+++ ClosestNumbers/BinaryIndexTree.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClosestNumbers
+{
+    /// <summary>
+    /// The sum operation where parent of index x is "x - (x & -x)".
+    /// The update operation where parent of index x is "x + (x & -x)".
+    /// </summary>
+    class BinaryIndexTree
+    {
+        // Returns sum of arr[0..index]. This function assumes
+        // that the array is preprocessed and partial sums of
+        // array elements are stored in BITree[].
+        public long getSum(long[] BITree, long index)
+        {
+            long sum = 0; // Iniialize result
+
+            // index in BITree[] is 1 more than the index in arr[]
+            index = index + 1;
+
+            // Traverse ancestors of BITree[index]
+            while (index > 0)
+            {
+                // Add current element of BITree to sum
+                sum += BITree[index];
+
+                // Move index to parent node in getSum View
+                index -= index & (-index);
+            }
+            return sum;
+        }
+
+        // Updates a node in Binary Index Tree (BITree) at given index
+        // in BITree.  The given value 'val' is added to BITree[i] and 
+        // all of its ancestors in tree.
+        public void updateBIT(long[] BITree, long n, long index, long val)
+        {
+            // index in BITree[] is 1 more than the index in arr[]
+            index = index + 1;
+
+            // Traverse all ancestors and add 'val'
+            while (index <= n)
+            {
+                // Add 'val' to current node of BI Tree
+                BITree[index] += val;
+
+                // Update index to that of parent in update View
+                index += index & (-index);
+            }
+        }
+
+        // Constructs and returns a Binary Indexed Tree for given
+        // array of size n.
+        public long[] constructBITree(long[] arr, long n)
+        {
+            // Create and initialize BITree[] as 0
+            long[] BITree = new long[n + 1];
+            for (long i = 1; i <= n; i++)
+                BITree[i] = 0;
+
+            // Store the actual values in BITree[] using update()
+            for (long i = 0; i < n; i++)
+                updateBIT(BITree, n, i, arr[i]);
+
+            // Uncomment below lines to see contents of BITree[]
+            //for (int i=1; i<=n; i++)
+            //      cout << BITree[i] << " ";
+
+            return BITree;
+        }
+
+        // Returns inversion count arr[0..n-1]
+        public long getInvCount(long[] arr, long n)
+        {
+            long invcount = 0; // Initialize result
+
+            // Find maximum element in arr[]
+            long maxElement = 0;
+            for (long i = 0; i < n; i++)
+                if (maxElement < arr[i])
+                    maxElement = arr[i];
+
+            // Create a BIT with size equal to maxElement+1 (Extra
+            // one is used so that elements can be directly be
+            // used as index)
+            long[] BIT = new long[maxElement + 1];
+            for (long i = 1; i <= maxElement; i++)
+                BIT[i] = 0;
+
+            // Traverse all elements from right.
+            for (long i = n - 1; i >= 0; i--)
+            {
+                // Get count of elements smaller than arr[i]
+                invcount += getSum(BIT, arr[i] - 1);
+
+                // Add current element to BIT
+                updateBIT(BIT, maxElement, arr[i], 1);
+            }
+
+            return invcount;
+        }
+    }
+}
---
+++ ClosestNumbers/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClosestNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int n = Convert.ToInt32(Console.ReadLine());
+            string[] arr_temp = Console.ReadLine().Split(' ');
+            //int[] arr = Array.ConvertAll(arr_temp, Int32.Parse);
+            //int[] result = closestNumbers(arr);
+            //Console.WriteLine(String.Join(" ", result));
+            //Console.WriteLine(String.Join(" ", InsertionSort(arr)));
+
+            #region
+            BinaryIndexTree bitree = new BinaryIndexTree();
+            long[] freq = { 2, 1, 3, 1, 2 };//// { 2, 1, 1, 3, 2, 3, 4, 5, 6, 7, 8, 9 };
+            long n = freq.Length;
+            Console.WriteLine(bitree.getInvCount(freq, n));
+            //Console.WriteLine(bitree.getSum(BITree, 3));
+            Array.ConvertAll(arr_temp, long.Parse);
+
+            //// Let use test the update operation
+            //freq[3] += 6;
+            //bitree.updateBIT(BITree, n, 3, 6); //Update BIT for above change in arr[]
+
+            //Console.WriteLine(bitree.getSum(BITree, 3));
+            #endregion
+
+            Console.Read();
+        }
+        static int[] closestNumbers(int[] arr)
+        {
+            Array.Sort(arr);
+            List<int> numbers = new List<int>();
+            int min = arr[1] - arr[0];
+            for (int i = 2; i < arr.Length; i++)
+            {
+                if (arr[i] - arr[i - 1] < min)
+                {
+                    min = arr[i] - arr[i - 1];
+                }
+            }
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] - arr[i - 1] == min)
+                {
+                    numbers.Add(arr[i - 1]);
+                    numbers.Add(arr[i]);
+                }
+            }
+
+            return numbers.ToArray();
+        }
+
+        private static int[] InsertionSort(int[] arr)
+        {
+            int count = 0;
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = i; j > 0; j--)
+                {
+                    if (arr[j] < arr[j - 1])
+                    {
+                        count++;
+                        var swap = arr[j];
+                        arr[j] = arr[j - 1];
+                        arr[j - 1] = swap;
+                    }
+                    else
+                        break;
+                }
+            }
+
+            return arr;
+        }
+
+    }
+}
